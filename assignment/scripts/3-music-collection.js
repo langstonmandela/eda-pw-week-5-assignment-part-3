7145091c57f8d3df7c0614d833cb94db91ee09c0
Langston Johnson <langstonjohnson@MacBook-Pro.local>
2023-12-11 09:05:40 -0600

    created a findByArtist function, and implemented test to prove it's working

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,10 +1,12 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+// Create an array called myCollection
 const myCollection = [];
 
 console.log( 'This is the first time displaying myCollection:', myCollection , 'it should be empty at this point!' );
 
+// Create an addtoCollection function
 function addToCollection( collection, title, artist, yearPublished ) {
   console.log ( 'in addToCollection, the number of records is: ', myCollection.length );
   let album = {
@@ -36,11 +38,30 @@ function showCollection( collection ) {
   }
 }
 
+// Test show collection:
 showCollection( myCollection );
 
 console.log( 'Adding an album to the MIX!---:', addToCollection( myCollection, 'Not Tight' , 'DOMi & JD Beck', 2022 ) );
 
 console.log( 'My updated collection: ', myCollection );
+
+// Create a function named findByArtist
+function findByArtist( collection, artist ) {
+  return collection.filter( album => album.artist === artist );
+}
+
+// Test the findByArtist function
+console.log( 'Find by John Mayer:', findByArtist(myCollection, 'John Mayer') );
+console.log( 'Find by unknown artist:', findByArtist(myCollection, 'Unknown Artist') );
+
+
+// Further Tests!!
+
+console.log( 'Adding an album to the MIX!---:', addToCollection( myCollection, 'Multi-Love', 'Unknown Mortal Orchestra', 2015 ) );
+
+console.log( 'Find by unknown artist:', findByArtist(myCollection, 'Unknown Artist') );
+console.log( 'Find by unknown artist:', findByArtist(myCollection, 'Unknown') );
+console.log( 'Find by Unknown Mortals!:', findByArtist(myCollection, 'Unknown Mortal Orchestra') );
 
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
